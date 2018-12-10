6dd8890f3fc4bb8f64d2dd62e6a2a461e297e697
sound_volume <niwano-satoshi342@g.ecc.u-tokyo.ac.jp>
2018-12-10 17:39:06 +0900

    js php

---
+++ phpsql.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("canvas");
+var mode = 1;
+function onClick(e){
+	var rect = e.target.getBoundingClientRect();
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+
+  var context = canvas.getContext('2d');
+  context.fillText("a",x,y);
+  fetch("http://m183.s25.xrea.com/test.php",{
+    method : 'POST',
+    headers : {
+      'Context-Type' : 'application/json'
+    },
+    body : JSON.stringify({
+      'id' : 0,
+      'op' : 0,
+      'data' : []
+    })
+  }).then(response =>{
+    context.fillText(response.statusText,x+10,y);
+    return response.text();
+  }).then(text =>{
+    context.fillText(text,x,y+10);
+  }).then(jsonData =>{
+    context.fillText("c",x+20,y);
+    context.fillText(jsonData,x,y);
+  });
+}
+canvas.addEventListener('click',onClick,false);
