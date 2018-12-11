f664978bee3b0f5a0379cdaf8e9a5038172fb93d
sound_volume <niwano-satoshi342@g.ecc.u-tokyo.ac.jp>
2018-12-11 17:49:30 +0900

    pjsonj

--- phpsql.js
+++ phpsql.js
@@ -6,24 +6,16 @@ function onClick(e){
   y = e.clientY - rect.top;
 
   var context = canvas.getContext('2d');
-  context.fillText("a",x,y);
   fetch("test.php",{
     method : 'POST',
-    headers : {
-      'Context-Type' : 'application/json'
-    },
     body : JSON.stringify({
       'id' : 0,
       'op' : 0,
       'data' : []
     })
   }).then(response =>{
-    context.fillText(response.statusText,x+10,y);
-    return response.text();
-  }).then(text =>{
-    context.fillText(text,x,y+10);
+    return response.json();
   }).then(jsonData =>{
-    context.fillText("c",x+20,y);
     context.fillText(jsonData,x,y);
   });
 }
--- test.php
+++ test.php
@@ -1,3 +1,4 @@
 <?php
 $data = json_decode(file_get_contents('php://input'));
-echo $data->id;
+$data->id += 1;
+echo json_encode($data);
