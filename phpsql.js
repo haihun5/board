58d0e9e93bf4cdb8d9c740be4a3c081f3da4ebc9
sound_volume <niwano-satoshi342@g.ecc.u-tokyo.ac.jp>
2018-12-14 14:52:06 +0900

    save png

--- phpsql.js
+++ phpsql.js
@@ -1,23 +1,97 @@
-var canvas = document.getElementById("canvas");
-var mode = 1;
-function onClick(e){
-	var rect = e.target.getBoundingClientRect();
+function leave(e){
+  mouse.isDrawing = false;
+
+  var imageData = context.toDataURL("image/png");
+  fetch("test.php", {
+    method: 'POST',
+    body: JSON.stringify({
+      'id': 0,
+      'op': 0,
+      'data': imageData
+    })
+  }).then(response => {
+    return response.json();
+  }).then(jsonData => {
+    context.fillText(jsonData, x, y);
+  });
+}
+
+window.onload = function() {
+  var canvas = document.getElementById('canvas');
+  if (!canvas || !canvas.getContext) {
+    return false;
+  }
+  var ctx = canvas.getContext('2d');
+
+  //マウスの座標を取得
+  var mouse = {
+    startX: 0,
+    startY: 0,
+    x: 0,
+    y: 0,
+    color: "black",
+    isDrawing: false
+  };
+  var borderWidth = 1;
+
+  canvas.addEventListener("mousemove", function(e) {
+    //2.マウスが動いたら座標値を取得
+    var rect = e.target.getBoundingClientRect();
+    mouse.x = e.clientX - rect.left - borderWidth;
+    mouse.y = e.clientY - rect.top - borderWidth;
+    /*
+    pageX[Y], offsetLeft[Top]を使う場合
+    mouse.x = e.pageX - canvas.offsetLeft - borderWidth;
+    mouse.y = e.pageY - canvas.offsetTop - borderWidth;
+    */
+
+    //4.isDrawがtrueのとき描画
+    if (mouse.isDrawing) {
+      ctx.beginPath();
+      ctx.moveTo(mouse.startX, mouse.startY);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.strokeStyle = mouse.color;
+      ctx.stroke();
+      mouse.startX = mouse.x;
+      mouse.startY = mouse.y;
+    }
+  });
+
+  //5.マウスを押したら、描画OK(myDrawをtrue)
+  canvas.addEventListener("mousedown", function(e) {
+    mouse.isDrawing = true;
+    mouse.startX = mouse.x;
+    mouse.startY = mouse.y;
+  });
+
+  //6.マウスを上げたら、描画禁止(myDrawをfalse)
+  canvas.addEventListener("mouseup", function(e) {
+    mouse.isDrawing = false;
+  });
+
+  canvas.addEventListener('mouseleave', function(e) {
+    mouse.isDrawing = false;
+  });
+}
+
+function onClick(e) {
+  var rect = e.target.getBoundingClientRect();
   x = e.clientX - rect.left;
   y = e.clientY - rect.top;
 
   var context = canvas.getContext('2d');
-	context.fillText(datas[mode],x,y+10);
-  fetch("test.php",{
-    method : 'POST',
-    body : JSON.stringify({
-      'id' : 0,
-      'op' : 0,
-      'data' : []
+  context.fillText(datas[mode], x, y + 10);
+  fetch("test.php", {
+    method: 'POST',
+    body: JSON.stringify({
+      'id': 0,
+      'op': 0,
+      'data': []
     })
-  }).then(response =>{
+  }).then(response => {
     return response.json();
-  }).then(jsonData =>{
-    context.fillText(jsonData,x,y);
+  }).then(jsonData => {
+    context.fillText(jsonData, x, y);
   });
 }
-canvas.addEventListener('click',onClick,false);
+//canvas.addEventListener('click', onClick, false);
--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
-$data = json_decode(file_get_contents('php://input'));
-$data->id += 1;
-echo json_encode($data);
+$json = json_decode(file_get_contents('php://input'));
+// baseFromJavascript will be the javascript base64 string retrieved of some way (async or post submited)
+//$baseFromJavascript = "data:image/png;base64,BBBFBfj42Pj4"; // $_POST['base64']; //your data in base64 'data:image/png....';
+// We need to remove the "data:image/png;base64,"
+$base_to_php = explode(',', $json->data);
+// the 2nd item in the base_to_php array contains the content of the image
+$data = base64_decode($base_to_php[1]);
+// here you can detect if type is png or jpg if you want
+$filepath = "/canvas/$json->id.png"; // or image.jpg
+
+// Save the image in a defined path
+file_put_contents($filepath,$data);
