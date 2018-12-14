f86d94d8fb8a34ff745becd5ba06750b3d956af7
sound_volume <niwano-satoshi342@g.ecc.u-tokyo.ac.jp>
2018-12-14 16:10:39 +0900

    coment out return

--- phpsql.js
+++ phpsql.js
@@ -1,93 +1,93 @@
-  var canvas = document.getElementById('canvas');
-  if (!canvas || !canvas.getContext) {
-    return false;
-  }
-  var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+//if (!canvas || !canvas.getContext) {
+//  return false;
+//}
+var ctx = canvas.getContext('2d');
 
-  var mouse = {
-    startX: 0,
-    startY: 0,
-    x: 0,
-    y: 0,
-    color: "black",
-    isDrawing: false
-  };
+var mouse = {
+  startX: 0,
+  startY: 0,
+  x: 0,
+  y: 0,
+  color: "black",
+  isDrawing: false
+};
 
-  function leave(e) {
-    mouse.isDrawing = false;
+function leave(e) {
+  mouse.isDrawing = false;
 
-    var imageData = ctx.toDataURL("image/png");
-    fetch("test.php", {
-      method: 'POST',
-      body: JSON.stringify({
-        'id': 0,
-        'op': 0,
-        'data': imageData
-      })
-    }).then(response => {
-      ctx.fillText('a', 0, 0);
-    });
-  }
+  var imageData = ctx.toDataURL("image/png");
+  fetch("test.php", {
+    method: 'POST',
+    body: JSON.stringify({
+      'id': 0,
+      'op': 0,
+      'data': imageData
+    })
+  }).then(response => {
+    ctx.fillText('a', 0, 0);
+  });
+}
 
-  window.onload = function() {
-    var borderWidth = 1;
+window.onload = function() {
+  var borderWidth = 1;
 
-    canvas.addEventListener("mousemove", function(e) {
-      //2.マウスが動いたら座標値を取得
-      var rect = e.target.getBoundingClientRect();
-      mouse.x = e.clientX - rect.left - borderWidth;
-      mouse.y = e.clientY - rect.top - borderWidth;
-      /*
-      pageX[Y], offsetLeft[Top]を使う場合
-      mouse.x = e.pageX - canvas.offsetLeft - borderWidth;
-      mouse.y = e.pageY - canvas.offsetTop - borderWidth;
-      */
+  canvas.addEventListener("mousemove", function(e) {
+    //2.マウスが動いたら座標値を取得
+    var rect = e.target.getBoundingClientRect();
+    mouse.x = e.clientX - rect.left - borderWidth;
+    mouse.y = e.clientY - rect.top - borderWidth;
+    /*
+    pageX[Y], offsetLeft[Top]を使う場合
+    mouse.x = e.pageX - canvas.offsetLeft - borderWidth;
+    mouse.y = e.pageY - canvas.offsetTop - borderWidth;
+    */
 
-      //4.isDrawがtrueのとき描画
-      if (mouse.isDrawing) {
-        ctx.beginPath();
-        ctx.moveTo(mouse.startX, mouse.startY);
-        ctx.lineTo(mouse.x, mouse.y);
-        ctx.strokeStyle = mouse.color;
-        ctx.stroke();
-        mouse.startX = mouse.x;
-        mouse.startY = mouse.y;
-      }
-    });
-
-    //5.マウスを押したら、描画OK(myDrawをtrue)
-    canvas.addEventListener("mousedown", function(e) {
-      mouse.isDrawing = true;
+    //4.isDrawがtrueのとき描画
+    if (mouse.isDrawing) {
+      ctx.beginPath();
+      ctx.moveTo(mouse.startX, mouse.startY);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.strokeStyle = mouse.color;
+      ctx.stroke();
       mouse.startX = mouse.x;
       mouse.startY = mouse.y;
-    });
+    }
+  });
 
-    //6.マウスを上げたら、描画禁止(myDrawをfalse)
-    canvas.addEventListener("mouseup", function(e) {
-      mouse.isDrawing = false;
-    });
+  //5.マウスを押したら、描画OK(myDrawをtrue)
+  canvas.addEventListener("mousedown", function(e) {
+    mouse.isDrawing = true;
+    mouse.startX = mouse.x;
+    mouse.startY = mouse.y;
+  });
 
-    canvas.addEventListener('mouseleave', leave);
-  }
+  //6.マウスを上げたら、描画禁止(myDrawをfalse)
+  canvas.addEventListener("mouseup", function(e) {
+    mouse.isDrawing = false;
+  });
 
-  function onClick(e) {
-    var rect = e.target.getBoundingClientRect();
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
+  canvas.addEventListener('mouseleave', leave);
+}
 
-    var context = canvas.getContext('2d');
-    context.fillText(datas[mode], x, y + 10);
-    fetch("test.php", {
-      method: 'POST',
-      body: JSON.stringify({
-        'id': 0,
-        'op': 0,
-        'data': []
-      })
-    }).then(response => {
-      return response.json();
-    }).then(jsonData => {
-      context.fillText(jsonData, x, y);
-    });
-  }
-  //canvas.addEventListener('click', onClick, false);
+function onClick(e) {
+  var rect = e.target.getBoundingClientRect();
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+
+  var context = canvas.getContext('2d');
+  context.fillText(datas[mode], x, y + 10);
+  fetch("test.php", {
+    method: 'POST',
+    body: JSON.stringify({
+      'id': 0,
+      'op': 0,
+      'data': []
+    })
+  }).then(response => {
+    return response.json();
+  }).then(jsonData => {
+    context.fillText(jsonData, x, y);
+  });
+}
+//canvas.addEventListener('click', onClick, false);
